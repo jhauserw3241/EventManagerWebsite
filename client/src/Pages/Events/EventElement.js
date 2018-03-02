44b5b9fd65aa2db628e1e8728def081d9bd57f20
Joy Hauser <joy.hauser@hotmail.com>
2018-03-02 00:16:11 -0600

    Update events page to change cards to list on toggle

--- client/src/Pages/Events/EventElement.js
+++ client/src/Pages/Events/EventElement.js
@@ -2,15 +2,27 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './../../CSS/Card.css';
 
-class EventElement extends Component {
-	render() {	
-		return (
-			<Link className="EventElement card" to={"/event/" + this.props.id}>
-				{this.props.name}<br />
-				{this.props.type}<br />
-				{this.props.completed_date}
-			</Link>
-		);
+class EventElement extends Component {s
+	render() {
+		if(this.props.org === "cards") {
+			return (
+				<Link className="EventElement card" to={"/event/" + this.props.id}>
+					{this.props.name}<br />
+					{this.props.type}<br />
+					{this.props.completed_date}
+				</Link>
+			);
+		} else if(this.props.org === "list") {
+			return (
+				<Link className="EventElement" to={"/event/" + this.props.id}>
+					{this.props.name}
+					{this.props.type}
+					{this.props.completed_date}
+				</Link>
+			);
+		} else {
+			return null;
+		}
 	}
 }
 
--- client/src/Pages/Events/Events.js
+++ client/src/Pages/Events/Events.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import EventElement from './EventElement';
+import Toggle from 'react-bootstrap-toggle';
+import { Button } from 'react-bootstrap';
 import fire from './../../fire';
 
 class Events extends Component {
@@ -7,8 +9,11 @@ class Events extends Component {
 		super(props);
 
 		this.state = {
-			events: []
+			events: [],
+			org: "list"
 		};
+
+		this.toggleOrganization = this.toggleOrganization.bind(this);
 	}
 	
 	componentDidMount() {
@@ -16,14 +21,42 @@ class Events extends Component {
 		eventsRef.orderByChild("name").on("value", (data) =>
 			this.setState({ events: data.val() }));
 	}
+
+	toggleOrganization(event) {
+		if(this.state.org === "cards") {
+			this.setState({ org: "list" });
+		} else if (this.state.org === "list") {
+			this.setState({ org: "cards" });
+		} else {
+			this.setState({ org: "list" });
+		}
+	}
 	
 	render() {
+		console.log(this.state.org);
+
+		var divStyle = {
+			float: "right"
+		}
 		return (
 			<div className="Events">			
 				<div className="container">
+					<div style={divStyle}>
+						<Button
+							onClick={this.toggleOrganization}
+							disabled={this.state.org === "list"}
+							bsStyle={this.state.org === "list" ? "primary" : "default"}
+							>List</Button>
+						<Button
+							onClick={this.toggleOrganization}
+							disabled={this.state.org === "cards"}
+							bsStyle={this.state.org === "cards" ? "primary" : "default"}
+							>Cards</Button>
+					</div>
 					<div className="list-container">
 						{this.state.events.map(event =>
 							<EventElement
+								org={this.state.org}
 								key={event.id}
 								id={event.id}
 								name={event.name}
