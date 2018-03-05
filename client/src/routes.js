e86f8328ba1feec4eae15e50cdb0f4a78d695b9f
Joy Hauser <joy.hauser@hotmail.com>
2018-03-05 14:33:17 -0600

    Add agenda page to routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Events from './Pages/Events/Events';
 import Event from './Pages/Event/Event';
+import Agenda from './Pages/Agenda/Agenda';
 import Login from './Pages/Login/Login';
 import Signup from './Pages/Signup/Signup';
 import Signout from './Pages/Signout/Signout';
@@ -14,6 +15,7 @@ export const Main = () => (
 			<Route exact path='/home' component={Home} />
 			<Route exact path='/events' component={Events} />
 			<Route exact path='/event/:id' component={Event} />
+			<Route exact path='/agenda/:id' component={Agenda} />
 			<Route exact path='/login' component={Login} />
 			<Route exact path='/signup' component={Signup} />
 			<Route exact path='/signout' component={Signout} />
