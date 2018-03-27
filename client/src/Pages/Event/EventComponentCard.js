edc5709e9c1c091bae7a33af2fe6b565053bfc84
Joy Hauser <joy.hauser@hotmail.com>
2018-03-26 21:08:57 -0500

    Add functionality to delete component

--- client/src/Pages/Event/EventComponentCard.js
+++ client/src/Pages/Event/EventComponentCard.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import fire from './../../fire';
 import './../../CSS/Card.css';
 
 class EventComponentCard extends Component {
+	constructor(props) {
+		super(props);
+
+		this.deleteComponent = this.deleteComponent.bind(this);
+	}
+
+	deleteComponent(event) {
+		event.preventDefault();
+
+		var self = this;
+
+		// Create event
+		fire.database().ref("events").child(this.props.event_id).child("components").child(this.props.component_id).remove();
+	}
+
 	render() {
 		var cardImgStyle = {
 			"backgroundColor": this.props.color
@@ -10,13 +26,16 @@ class EventComponentCard extends Component {
 
 		return (
 			<Link className="EventComponentCard card" to={this.props.link}>
-                {this.props.children}
-
 				<div className="card-img" style={cardImgStyle}></div>
 				<div className="card-text">
 					{this.props.name}
 				</div>
-				<div className="card-mod-btns">
+				<div className="card-btns">
+					<button
+						className="btn btn-danger card-delete-btn"
+						onClick={this.deleteComponent}>
+						Delete
+					</button>
 				</div>
 			</Link>
 		);
