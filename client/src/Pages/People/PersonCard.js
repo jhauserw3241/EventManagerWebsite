694056e1a015bfd35311872c6f574f0cc064c566
Joy Hauser <joy.hauser@hotmail.com>
2018-04-16 22:32:42 -0500

    Add fields on the people modal to see associated agencies and shared events

--- client/src/Pages/People/PersonCard.js
+++ client/src/Pages/People/PersonCard.js
@@ -1,11 +1,63 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import fire from './../../fire';
 import './../../CSS/Card.css';
 
 class PersonCard extends Component {
-	render() {
-        console.log(this.props.pic);
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            all_agencies: [],
+            person_agencies: [],
+            events: [],
+        }
+
+        this.getAllAgencies = this.getAllAgencies.bind(this);
+        this.getAssociatedAgencies = this.getAssociatedAgencies.bind(this);
+        this.getSharedEvents = this.getSharedEvents.bind(this);
+    }
+
+    componentDidMount() {
+        this.getAllAgencies();
+        this.getAssociatedAgencies();
+        this.getSharedEvents();
+    }
+
+    getAllAgencies() {
+        var self = this;
+        fire.database().ref("agencies").on("value", function(data) {
+            console.log(data.val());
+            var agency_names = [];
+            var agencies = data.val() ? Object.values(data.val()) : [];
+
+            for(var agency_id in agencies) {
+                var agency = agencies[agency_id];
+                agency_names.push(agency.name);
+            }
+
+            console.log(agency_names);
+            self.setState({ all_agencies: agency_names });
+        });
+    }
+
+    getAssociatedAgencies() {
+        var self = this;
+        fire.database().ref("users").child(this.props.id).child("agencies").on("value", function(data) {
+            console.log(data.val());
+            self.setState({ person_agencies: data.val() ? Object.values(data.val()) : [] });
+        });
+    }
+
+    getSharedEvents() {
+        var self = this;
+        fire.database().ref("users").child(this.props.id).child("events").on("value", function(data) {
+            console.log(data.val());
+            self.setState({ events: data.val() ? Object.values(data.val()) : [] });
+        });
+    }
+
+	render() {
 		var cardImgStyle = {
             backgroundImage: `url(${this.props.pic})`
 		}
@@ -81,6 +133,25 @@ class PersonCard extends Component {
                                         value={this.props.notes}
                                         disabled={true}
                                         ></textarea>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="agencies">Associated Agencies:</label>
+                                    <input
+                                        name="agencies"
+                                        className="form-control"
+                                        value={this.state.person_agencies.join(", ")}
+                                        disabled={true}
+                                        />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="events">Events:</label>
+                                    <input
+                                        type="text"
+                                        name="events"
+                                        className="form-control"
+                                        value={this.state.events.join(", ")}
+                                        disabled={true}
+                                        />
                                 </div>
                             </div>
                             <div className="modal-footer">
