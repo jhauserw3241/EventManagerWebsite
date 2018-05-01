6f05c453825a5ea91f4be9eea8028c5d2b1e7261
Joy Hauser <joy.hauser@hotmail.com>
2018-05-01 00:23:00 -0500

    Refactor add product modal

--- client/src/Pages/Products/Products.js
+++ client/src/Pages/Products/Products.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ItemsDashboard from './../ItemsDashboard/ItemsDashboard';
+import AddProductModal from './AddProductModal';
 import { Redirect } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import fire from './../../fire';
@@ -11,12 +12,9 @@ class Products extends Component {
 
 		this.state = {
 			products: {},
-			name: "",
-			type: "",
 			formError: ""
 		};
 
-		this.addProduct = this.addProduct.bind(this);
 		this.deleteProduct = this.deleteProduct.bind(this);
 	}
 	
@@ -53,46 +51,6 @@ class Products extends Component {
 		});
 	}
 
-	addProduct(event) {
-		event.preventDefault();
-
-		if(!fire.auth().currentUser) {
-			return;
-		}
-
-		var self = this;
-
-		var owner_id = fire.auth().currentUser.uid;
-		fire.database().ref("users").child(owner_id).once("value", function(data) {
-			var user = data.val();
-			var user_name = user.first_name + " " + user.last_name;
-
-			var partnersList = {};
-			partnersList[owner_id] = {
-				name: user_name,
-				role: "Owner",
-				priv: "Owner",
-			}
-
-			// Create event
-			var curProductRef = fire.database().ref("products").push();
-			var product_id = curProductRef.key;
-			curProductRef.set({
-				id: product_id,
-				name: self.state.name,
-				type: self.state.type,
-				owner_id: owner_id,
-				partners: partnersList,
-				color: "#"+((1<<24)*Math.random()|0).toString(16), // Generate random color
-			});
-
-			// Add this product to the list of products that is being tracked for the current user
-			var updates = {};
-			updates['/users/' + owner_id + '/products/' + product_id] = product_id;
-			fire.database().ref().update(updates);
-		});
-	}
-
 	deleteProduct(event, id) {
 		event.preventDefault();
 
@@ -113,60 +71,7 @@ class Products extends Component {
 		return (
 			<div className="Products">
 				<div className="container">
-					<div
-						className="modal fade"
-						id="addProductModal"
-						tabIndex="-1"
-						role="dialog"
-						data-backdrop="static"
-						data-keyboard={false}
-						aria-labelledby="addProductModalTitle"
-						aria-hidden="true">
-						<div className="modal-dialog" role="document">
-							<div className="modal-content">
-								<div className="modal-header">
-									<h5 className="modal-title" id="addProductModalTitle">Add Product</h5>
-									<button type="button" className="close" data-dismiss="modal" aria-label="Close">
-										<span aria-hidden="true">&times;</span>
-									</button>
-								</div>
-								<div className="modal-body">
-									<div className="form-group">
-										<label htmlFor="name">Name:</label>
-										<input
-											type="text"
-											name="name"
-											className="form-control"
-											onChange={(event) => this.setState({name: event.target.value})}
-											required />
-									</div>
-									<div className="form-group">
-										<label htmlFor="type">Type:</label>
-										<select
-											name="type"
-											className="form-control"
-											onChange={(event) => this.setState({type: event.target.value})}
-											required>
-											<option>Not Specified</option>
-											<option>Publication</option>
-											<option>Video</option>
-											<option>Webinar</option>
-											<option>Miscellaneous</option>
-										</select>
-									</div>
-								</div>
-								<div className="modal-footer">
-									<button
-										type="button"
-										className="btn btn-primary"
-										data-dismiss="modal"
-										onClick={this.addProduct}>
-										Submit
-									</button>
-								</div>
-							</div>
-						</div>
-					</div>
+					<AddProductModal />
 	
 					{ (this.state.formError !== "") ?
 						<div class="alert alert-danger">
