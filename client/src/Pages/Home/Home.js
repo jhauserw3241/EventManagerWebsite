2b4c1d44a539ac722ca6cd8155cebf04b5e9ba18
Joy Hauser <joy.hauser@hotmail.com>
2018-05-02 22:18:33 -0500

    Update home page text to explain the website

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -7,13 +7,13 @@ class Home extends Component {
 			<div className="Home">
 				<div className="container">
 					<div className="content">
-						This is the home page. Welcome!
-
-						<LoginRequired>
-							<br />
-							<br />
-							Secrets!!
-						</LoginRequired>
+						Welcome to the event planner website! This website was created to help you plan large
+						events or the creation of products. This website was created around the idea of providing
+						you witl the freedom to add different types of information. Additionally, this website will
+						allow you to share information with other people.<br />
+						<br/>
+						To be able to use the features on this website you need to log in. If you do not have
+						an account, please sign up for one on the sign up page.
 					</div>
 				</div>
 			</div>
