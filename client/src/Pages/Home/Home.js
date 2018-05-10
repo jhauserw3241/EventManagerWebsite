342566db6903d97377cf0852248da988cb12d554
Joy Hauser <joy.hauser@hotmail.com>
2018-05-09 22:36:10 -0500

    Fix grammar and wording errors on the home page

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -7,8 +7,8 @@ class Home extends Component {
 				<div className="container">
 					<div className="content">
 						Welcome to the event planner website! This website was created to help you plan large
-						events or the creation of products. This website was created around the idea of providing
-						you witl the freedom to add different types of information. Additionally, this website will
+						events or the creation of products. It was created around the idea of providing
+						you with the freedom to add different types of information. Additionally, this website will
 						allow you to share information with other people.<br />
 						<br/>
 						To be able to use the features on this website you need to log in. If you do not have
